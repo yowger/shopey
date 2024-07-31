9d1934420e9ded07393664bc1872aea6d3b56e6c
Roger Pantil <rogerpantilyowger@gmail.com>
2024-08-01 02:15:47 +0800

    updated auth config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from "drizzle-kit"
+import * as dotenv from "dotenv"
+
+dotenv.config({
+    path: ".env.local",
+})
 
 export default defineConfig({
     dialect: "postgresql",
     schema: "./src/server/schema.ts",
     out: "./src/server/migrations",
+    dbCredentials: {
+        url: process.env.AUTH_DRIZZLE_URL!,
+    },
 })
